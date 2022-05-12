45fe025b9462ac9836bcd74de996325c1043f02b
Matheus <matheuscarretta@gmail.com>
2022-05-12 16:55:39 -0300

    feat: adiciona testes da funcao getAll products de services

--- test/unit/services/productsServices.js
+++ test/unit/services/productsServices.js
@@ -1,1 +1,36 @@
 const { expect } = require('chai');
+const sinon = require('sinon');
+
+const productsService = require('../../../services/productsService');
+const connection = require('../../../models/connection')
+
+describe('Quando busca todos os produtos', () => {
+  const payloadProduct = [[{
+		"id": 1,
+		"name": "Martelo de Thor",
+		"quantity": 10
+	}]];
+
+  before(async () => {
+    const execute = payloadProduct;
+
+    sinon.stub(connection, 'execute').resolves(execute);
+  });
+
+  after(async () => {
+    connection.execute.restore();
+  });
+
+    it('retorna um array', async () => {
+      const response = await productsService.getAll();
+
+      expect(response).to.be.a('array');
+    });
+
+    it('o array possui objetos com as propriedades id, name, quantity', async () => {
+      const [response] = await productsService.getAll();
+
+      expect(response).to.have.all.keys('id', 'name', 'quantity');
+    });
+
+});
