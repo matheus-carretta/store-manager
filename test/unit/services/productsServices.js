7948fc5153c04e40bdd9b06e6eec769c6dfe04cf
Matheus <matheuscarretta@gmail.com>
2022-05-13 21:49:57 -0300

    feat: testes da funcao getProduct na camada services

--- test/unit/services/productsServices.js
+++ test/unit/services/productsServices.js
@@ -32,3 +32,60 @@ describe('Ao executar o getAll da service', () => {
     });
 
 });
+
+describe("Ao usar o getProduct da services", () => {
+  describe('e não encontra um produto', () => {
+    const payload = [];
+    const id = 10;
+
+    before(async () => {
+      sinon.stub(productsModel, 'getProduct').resolves(payload);
+    });
+
+    after(async () => {
+      productsModel.getProduct.restore();
+    })
+
+    it('retorna um array', async () => {
+      const response = await productsService.getProduct(id);
+
+      expect(response).to.be.a('array');
+    })
+
+    it('e esse array está vazio', async () => {
+      const response = await productsService.getProduct(id);
+
+      expect(response).to.be.empty;
+    })
+  })
+
+  describe('quando encontra um produto', () => {
+    const id = 1;
+    const payload = {
+      "id": 1,
+      "name": "Martelo de Thor",
+      "quantity": 10
+    };
+
+    before(async () => {
+      sinon.stub(productsModel, 'getProduct').resolves(payload);
+    })
+
+    after(async () => {
+      productsModel.getProduct.restore();
+    })
+
+    it('retorna um objeto', async () => {
+      const response = await productsService.getProduct(id);
+
+      expect(response).to.be.a('object');
+    })
+
+    it('o objeto possui as chaves id, name e quantity', async () => {
+      const response = await productsService.getProduct(id);
+
+      expect(response).to.have.all.keys('id', 'name', 'quantity');
+    })
+
+  })
+})
