a2fbc50c70d5f052fbf29e398df1b541edfbed6a
Matheus <matheuscarretta@gmail.com>
2022-05-15 16:54:13 -0300

    feat: testa funcao getSale da camada de model

--- test/unit/models/salesModels.js
+++ test/unit/models/salesModels.js
@@ -34,3 +34,35 @@ describe('Ao executar o getAll da model', () => {
     expect(response).to.have.all.keys('sale_id', 'product_id', 'quantity', 'id', 'date');
   })
 })
+
+describe('Ao executar o getAll da model', () => {
+  const payload = [[{
+      sale_id: 1,
+      product_id: 1,
+      quantity: 5,
+      id: 1,
+      date: '2022-05-10T23:13:55.000Z'
+  }]];
+  const id = 1;
+
+  before(async () => {
+    sinon.stub(connection, 'execute').resolves(payload);
+  });
+
+  after(async () => {
+    connection.execute.restore();
+  });
+
+  it('Retorna um array com uma venda e todos os produtos vendidos', async () => {
+    const response = await salesModel.getSale(id);
+
+    expect(response).to.be.a('array');
+  });
+
+  it('cada objeto possui a chave sale_id, product_id, quantity, id e date', async () => {
+    const [response] = await salesModel.getSale(1);
+
+    expect(response).to.have.all.keys('sale_id', 'product_id', 'quantity', 'id', 'date');
+  });
+
+});
